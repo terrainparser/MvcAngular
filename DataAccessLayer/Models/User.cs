a6df45addd179431050de49c5fc2886c4990500c
terrainparser <harishdotnet6@gmail.com>
2016-07-28 17:35:33 -0400

    EF code first

--- DataAccessLayer/Models/User.cs
+++ DataAccessLayer/Models/User.cs
@@ -8,27 +8,34 @@ namespace DataAccessLayer.Models
 {
     public class User
     {
-        public User() 
+        public User()
         {
-        
+
         }
+        public int UserId { get; set; }
+
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
 
         public string Email { get; set; }
 
-        public Review Review { get; set; }
+       // public Review Review { get; set; }
     }
 
-    public class Review 
+    public class Review
     {
-        public Review() 
+        public Review()
         {
 
         }
+        public int ReviewId { get; set; }
+
         public int Rating { get; set; }
 
         public string Comment { get; set; }
+
+        public User User { get; set; }
+        //List<User> Users { get; set; }
     }
 }
---
+++ Tests.DataAccessLayer/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataAccessLayer.Repositories;
+using DataAccessLayer.Models;
+
+namespace Tests.DataAccessLayer
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestInsert()
+        {
+            var userRepository = new UserRepository();
+            userRepository.AddUser(new User { FirstName = "H", LastName = "B", Email = "h.b@gmail.com" });
+
+        }
+    }
+}
